6883c25585cc4d9665be320ac09aa210a46d6019
Andrea Calabrese <andrea.calabrese@amarulasolutions.com>
2024-04-30 13:42:35 +0200

    Added layouts for slides
    Added and modified present layouts for slides. This adds:
    - default settings for layouts
    - automatic page count
    
    Signed-off-by: Andrea Calabrese <andrea.calabrese@amarulasolutions.com>

--- template/layouts/section.vue
+++ template/layouts/section.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="slidev-layout section">
-    <img src="../images/amarula.svg" class="logo-topright"/>
     <div class="my-auto">
       <slot />
     </div>
   </div>
-  <table class="bottompanel">
-      <tr>
-        <th style="text-align:left; padding-left:8px;">
-          <a href="https://www.amarulasolutions.com">www.amarulasolutions.com</a>
-        </th>
-        <th>
-          <SlideCurrentNo />
-        </th>
-      </tr>
-  </table>
+  <div class="bottompanel">
+    <SlideCurrentNo />
+  </div>
 </template>
