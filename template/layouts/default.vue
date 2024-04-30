9597e98ec320a6b9a51a5a1488e92cfd3f709643
Andrea Calabrese <andrea.calabrese@amarulasolutions.com>
2024-04-30 15:47:59 +0200

    Added layout from regmap presentation
    Added layouts useful for the regmap presentation. Also modified the
    original layout, removing the gray background and keeping it white.
    
    Signed-off-by: Andrea Calabrese <andrea.calabrese@amarulasolutions.com>

--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
-        <div class="slidev-layout default">
-          <div class="my-auto">
-            <slot />
-          </div>
-        </div>
-        <div class="bottompanel">
-          <SlideCurrentNo />
-        </div>
-      </template>
+  <div class="slidev-layout default">
+    <div class="my-auto">
+      <slot />
+    </div>
+  </div>
+  <div class="bottompanel">
+    <SlideCurrentNo />
+  </div>
+</template>
--- template/layouts/two-cols.vue
+++ template/layouts/two-cols.vue
@@ -14,19 +14,11 @@ const props = defineProps({
 <template>
   <div class="grid grid-cols-2 w-full h-full">
     <div class="slidev-layout default image-right" :class="props.class">
-    <img src="../images/amarula.svg" class="logo-topright"/>
       <slot />
     </div>
     <div class="my-14 mr-14" :style="style" />
   </div>
-  <table class="bottompanel">
-      <tr>
-        <th style="text-align:left; padding-left:8px;">
-          <a href="https://www.amarulasolutions.com">www.amarulasolutions.com</a>
-        </th>
-        <th>
-          <SlideCurrentNo />
-        </th>
-      </tr>
-  </table>
+  <div class="bottompanel">
+    <SlideCurrentNo />
+  </div>
 </template>
