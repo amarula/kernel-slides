8129589870b8cd25f511934185af84699bae1220
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2024-06-05 14:22:50 +0200

    template: layouts: Change last slide
    Add bottom panel to last slide, without slide number and with a centered
    website link.
    Remove thank you in all caps, and add amarula logo.
    
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

--- template/layouts/last-slide.vue
+++ template/layouts/last-slide.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="slidev-layout last-slide">
-    <img src="../images/amarula.svg" class="logo-topright"/>
-    <h1>THANK YOU!</h1>
+  <div class="slidev-layout h-full grid place-content-center last-slide">
+    <img src="../images/amarula-horizontal.png" class="logo"/>
   </div>
+  <table class="bottompanel">
+    <tr>
+      <th style="text-align:center; padding-left:8px; font-weight:400">
+        <a href="https://www.amarulasolutions.com">https://www.amarulasolutions.com</a>
+      </th>
+    </tr>
+  </table>
 </template>
