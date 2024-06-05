9b12d2414e1ddc29355fc19cf47a4f00bde5f4d4
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2024-06-05 10:59:07 +0200

    template: styles: Make bottom panel prettier
    Make bottom panel thinner, make bottom panel text smaller in size and
    thinner.
    Also remove redundant lines that appeared twice.
    
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

--- template/layouts/section.vue
+++ template/layouts/section.vue
@@ -6,11 +6,11 @@
   </div>
   <table class="bottompanel">
       <tr>
-        <th style="text-align:left; padding-left:8px;">
+        <th style="text-align:left; padding-left:8px; font-weight:400">
           <a href="https://www.amarulasolutions.com">https://www.amarulasolutions.com</a>
         </th>
         <th>
-          <SlideCurrentNo />
+          <SlideCurrentNo style="font-weight:400"/>
         </th>
       </tr>
   </table>
