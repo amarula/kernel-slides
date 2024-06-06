e8949732a50e23477cadb240f5f092c0af30ed96
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2024-06-06 16:00:34 +0200

    template: layout: Make Amarula logos responsive
    Make Amarula logos render conditionally, so that the logo is always
    readable.
    The white logo will display on dark background, and the black logo will
    display on light background.
    
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

--- template/layouts/cover.vue
+++ template/layouts/cover.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { isDark } from '@slidev/client/logic/dark.ts'
+</script>
+
 <template>
   <div class="slidev-layout cover">
-    <img src="../images/amarula-horizontal.png" class="logo"/>
+    <img src="../images/amarula-horizontal-white.png" class="logo" v-if="isDark" />
+    <img src="../images/amarula-horizontal.png" class="logo" v-else />
     <div class="my-auto w-full">
       <slot />
     </div>
--- template/layouts/last-slide.vue
+++ template/layouts/last-slide.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { isDark } from '@slidev/client/logic/dark.ts'
+</script>
+
 <template>
   <div class="slidev-layout h-full grid place-content-center last-slide">
-    <img src="../images/amarula-horizontal.png" class="logo"/>
+    <img src="../images/amarula-horizontal-white.png" class="logo" v-if="isDark" />
+    <img src="../images/amarula-horizontal.png" class="logo" v-else />
   </div>
   <table class="bottompanel">
     <tr>
